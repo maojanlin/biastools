7bed4cb4d9b6f36d584d001c540e213c92752205
Sheila Iyer <sheilaiyer01@outlook.com>
2021-02-09 10:10:42 -0500

    creates VCF file with HET sites

---
+++ VCF_Processing.py
@@ -0,0 +1,45 @@
+#program to find HET sites from VCF
+import argparse
+
+def main(fn_vcf, fn_output):
+    #t = time.time()
+    file = open(fn_vcf, 'r')
+    f = open(fn_output, 'a+')
+
+    index = -1
+    toAdd = []
+    should = True
+    for line in file:
+        if should and line.startswith("##"):
+            should = should
+            f.write(line)
+        else:
+            if line.startswith("#"):
+                categories = line.split()
+                for i in range(len(categories)):
+                    if categories[i] == 'NA12878':
+                        index = i
+                toAdd = [categories[i] for i in range(8)]
+                toAdd.append(categories[index])
+                f.write(line)
+	    else:
+                spl = line.split()
+
+                if len(set(spl[index].split("|")) )>1:
+                    toAdd = [spl[i] for i in range(8)]
+                    toAdd.append(spl[index])
+                    f.write("\t".join(toAdd))
+                    f.write("\n")
+        #print("time: ", time.time() - t)
+    f.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--vcf', help='vcf file for chromosomes')
+    parser.add_argument('-o', '--out', help='output vcf file with HET sites')
+    args = parser.parse_args()
+    fn_vcf = args.vcf
+    fn_output = args.out
+    print('vcf', fn_vcf)
+    print('output', fn_output)
+    main(fn_vcf, fn_output)
