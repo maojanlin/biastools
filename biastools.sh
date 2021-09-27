712482bca116ffa285fbb16f3970cf72e473a5e7
mlin77 <mlin77@login03.rockfish.cluster>
2021-09-27 10:52:56 -0400

    adding indexing command and modify tools path

--- biastools.sh
+++ biastools.sh
@@ -1,15 +1,15 @@
-#bgzip -c renamed_chr21_NA12878.vcf > renamed_chr21_NA12878.vcf.gz
-#
-#bcftools index renamed_chr21_NA12878.vcf.gz
-#bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapA.fa -H 1 renamed_chr21_NA12878.vcf.gz
-#bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapB.fa -H 2 renamed_chr21_NA12878.vcf.gz
-#
-#mason_simulator -ir GRCh38_chr21.hapA.fa -o hapA_1.fq -or hapA_2.fq -oa hapA.sam -n 3600000
-#mason_simulator -ir GRCh38_chr21.hapB.fa -o hapB_1.fq -or hapB_2.fq -oa hapB.sam -n 3600000
+bgzip -c renamed_chr21_NA12878.vcf > renamed_chr21_NA12878.vcf.gz
 
-#bowtie2-build-s  GRCh38_chr21.fa ./index/GRCh38_chr21/chr21_index
-#bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapA_1.fq -2 hapA_2.fq -S hapA.bt2.sam
-#bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapB_1.fq -2 hapB_2.fq -S hapB.bt2.sam
+bcftools index renamed_chr21_NA12878.vcf.gz
+bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapA.fa -H 1 renamed_chr21_NA12878.vcf.gz
+bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapB.fa -H 2 renamed_chr21_NA12878.vcf.gz
+
+mason_simulator -ir GRCh38_chr21.hapA.fa -o hapA_1.fq -or hapA_2.fq -oa hapA.sam -n 3600000
+mason_simulator -ir GRCh38_chr21.hapB.fa -o hapB_1.fq -or hapB_2.fq -oa hapB.sam -n 3600000
+
+bowtie2-build-s  GRCh38_chr21.fa ./index/GRCh38_chr21/chr21_index
+bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapA_1.fq -2 hapA_2.fq -S hapA.bt2.sam
+bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapB_1.fq -2 hapB_2.fq -S hapB.bt2.sam
 
 samtools sort -@ 16 -o hapA.bt2.sorted.sam hapA.bt2.sam
 samtools sort -@ 16 -o hapB.bt2.sorted.sam hapB.bt2.sam
