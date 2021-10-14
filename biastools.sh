c947beb6c7a4b38b2d59e246ae0ccb473f4f7ddf
mlin77 <mlin77@login02.rockfish.cluster>
2021-10-14 17:34:16 -0400

    adding the bcftools norm for bcftools consensus

--- biastools.sh
+++ biastools.sh
@@ -1,25 +1,31 @@
-#bgzip -c renamed_chr21_NA12878.vcf > renamed_chr21_NA12878.vcf.gz
-#bcftools index renamed_chr21_NA12878.vcf.gz
-#
-#bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapA.fa -H 1 renamed_chr21_NA12878.vcf.gz
-#bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapB.fa -H 2 renamed_chr21_NA12878.vcf.gz
-#samtools faidx GRCh38_chr21.hapA.fa
-#samtools faidx GRCh38_chr21.hapB.fa
-#
-#mason_simulator -ir GRCh38_chr21.hapA.fa -o hapA_1.fq -or hapA_2.fq -oa hapA.sam -n 3600000
-#mason_simulator -ir GRCh38_chr21.hapB.fa -o hapB_1.fq -or hapB_2.fq -oa hapB.sam -n 3600000
-#
-#bowtie2-build GRCh38_chr21.fa ./index/GRCh38_chr21/chr21_index
-#bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapA_1.fq -2 hapA_2.fq -S hapA.bt2.sam
-#bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapB_1.fq -2 hapB_2.fq -S hapB.bt2.sam
+samtools faidx GRCh38_chr21.fa
 
-#samtools sort -@ 16 hapA.bt2.sam -o hapA.bt2.sorted.bam
-#samtools sort -@ 16 hapB.bt2.sam -o hapB.bt2.sorted.bam
-#
-#samtools merge -f -r bt2.sorted.bam hapA.bt2.sorted.bam hapB.bt2.sorted.bam
-#
+bgzip -c renamed_chr21_NA12878.vcf > renamed_chr21_NA12878.vcf.gz
+bcftools norm -f GRCh38_chr21.fa renamed_chr21_NA12878.vcf.gz -m +any -Oz -o normalized_chr21_NA12878.vcf.gz
+bcftools index normalized_chr21_NA12878.vcf.gz
+
+echo "[BIASTOOLS] Generate haplotype consensus reference sequence"
+bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapA.fa -H 1 normalized_chr21_NA12878.vcf.gz
+bcftools consensus -f GRCh38_chr21.fa -o GRCh38_chr21.hapB.fa -H 2 normalized_chr21_NA12878.vcf.gz
+samtools faidx GRCh38_chr21.hapA.fa
+samtools faidx GRCh38_chr21.hapB.fa
+
+echo "[BIASTOOLS] Simulate sequences"
+mason_simulator -ir GRCh38_chr21.hapA.fa -o hapA_1.fq -or hapA_2.fq -oa hapA.sam -n 3600000
+mason_simulator -ir GRCh38_chr21.hapB.fa -o hapB_1.fq -or hapB_2.fq -oa hapB.sam -n 3600000
+
+echo "[BIASTOOLS] Align sequences to the original reference"
+bowtie2-build GRCh38_chr21.fa ./index/GRCh38_chr21/chr21_index
+bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapA_1.fq -2 hapA_2.fq -S hapA.bt2.sam
+bowtie2 -p 32 -x ./index/GRCh38_chr21/chr21_index -1 hapB_1.fq -2 hapB_2.fq -S hapB.bt2.sam
+
+samtools sort -@ 16 hapA.bt2.sam -o hapA.bt2.sorted.bam
+samtools sort -@ 16 hapB.bt2.sam -o hapB.bt2.sorted.bam
+
+samtools merge -f -r bt2.sorted.bam hapA.bt2.sorted.bam hapB.bt2.sorted.bam
+
 echo "[BIASTOOLS] Filter the heterozygous site in vcf file"
-python3 filter_het_VCF.py -v renamed_chr21_NA12878.vcf.gz -o chr21_het.vcf.gz
+python3 filter_het_VCF.py -v normalized_chr21_NA12878.vcf.gz -o chr21_het.vcf.gz
 tabix -p vcf chr21_het.vcf.gz
 
 echo "[BIASTOOLS] Intersect the bam file and vcf file"
