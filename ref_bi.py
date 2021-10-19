d848773c1ee13d4be146cef9c6a6d0c5407be71e
mlin77 <mlin77@login03.rockfish.cluster>
2021-10-19 17:28:20 -0400

    adding extensions for gap bias analysis

--- ref_bi.py
+++ ref_bi.py
@@ -11,29 +11,72 @@ def parse_vcf_to_dict(fn_vcf):
     print("Start parsing the vcf file", fn_vcf)
     in_vcf_file = pysam.VariantFile(fn_vcf, 'r')
     count_het = 0
-    chr_vcf = {}
+    chr_SNP_vcf = {}
+    chr_gap_vcf = {}
     for segment in in_vcf_file:
         ref_name = segment.contig
         pos      = segment.pos - 1
         ref      = segment.ref
         list_alt = segment.alts
         hap_info = str(segment).split()[9] # "0|0", "1|0", "0|1" tag
-        if len(ref) > 1 or len(list_alt[0]) > 1:  # TODO skip all the multiple bases variant site
-            continue
-        
-        if ref_name not in chr_vcf.keys():
-            chr_vcf[ref_name] = [[pos, ref, list_alt, hap_info]]
+        if len(ref) > 1 or len(list_alt[0]) > 1:  # separate SNP and gap sites
+            if ref_name not in chr_gap_vcf.keys():
+                chr_gap_vcf[ref_name] = [[pos, ref, list_alt, hap_info]]
+            else:
+                chr_gap_vcf[ref_name].append([pos, ref, list_alt, hap_info])
         else:
-            chr_vcf[ref_name].append([pos, ref, list_alt, hap_info])
+            if ref_name not in chr_SNP_vcf.keys():
+                chr_SNP_vcf[ref_name] = [[pos, ref, list_alt, hap_info]]
+            else:
+                chr_SNP_vcf[ref_name].append([pos, ref, list_alt, hap_info])
     in_vcf_file.close()
-    return chr_vcf
-    
+    return chr_SNP_vcf, chr_gap_vcf
+   
+
+def add_gap(dict_var_gap, idx, pattern):
+    """
+    dict_var_gap{}
+    - keys: position
+    - values: dict_pattern{}
+              - keys: pattern
+              - values: occurance
+    """
+    if dict_var_gap.get(idx):
+        if dict_var_gap[idx].get(pattern):
+            dict_var_gap[idx][pattern] += 1
+        else:
+            dict_var_gap[idx][pattern] = 1
+    else:
+        dict_var_gap[idx] = {pattern: 1}
+
+
+def warp_by_cigar(cigar_tuples, sequence, query_name, dict_var_gap, start_pos):
+    mod_sequence = ""
+    mod_id = 0
+    for pair_info in cigar_tuples:
+        code, runs = pair_info
+        if code ==0: # M
+            mod_sequence += sequence[mod_id:mod_id + runs]
+            mod_id += runs
+        elif code == 1: # I
+            add_gap(dict_var_gap, start_pos+mod_id-1, ('I', sequence[mod_id:mod_id+runs]))
+            mod_id += runs
+        elif code == 2: # D
+            add_gap(dict_var_gap, start_pos+mod_id-1, ('D', runs))
+            mod_sequence += '-'*runs
+        elif code == 4 or code == 5: # S or H, pysam already parsed
+            pass
+        else:
+            print ("ERROR: unexpected cigar code in sequence", query_name)
+    return mod_sequence
+
 
 def parse_sam_to_dict(fn_sam):
     """ load the sam file to a dict{list_info} according to chromosome name """
     print("Start parsing the sam file", fn_sam)
     in_sam_file = pysam.AlignmentFile(fn_sam, "r")
     chr_sam = {}
+    dict_chr_var_gap = {}
     for segment in in_sam_file:
         flag = segment.flag
         if (flag & 4): # bitwise AND 4, segment unmapped
@@ -42,54 +85,41 @@ def parse_sam_to_dict(fn_sam):
         if ref_name.endswith('A') or ref_name.endswith('B'): # ignores haplotype suffixes
             ref_name = ref_name[:-1]
             
-        parsed_cigar = segment.cigartuples
+        seq_name  = segment.query_name
+        cigar_tuples = segment.cigartuples
         start_pos = segment.reference_start # start position in genome coordiante
         mapq      = segment.mapping_quality
         rg_tag    = segment.get_tag("RG")
         
-        sequence  = segment.query_alignment_sequence
-        mod_sequence = ""
+        sequence  = segment.query_alignment_sequence # aligned sequence without SoftClip part
+        if ref_name not in chr_sam.keys(): # initialize the dictionary according to chromosome name
+            chr_sam[ref_name] = [[], [], [], []]
+            dict_chr_var_gap[ref_name] = {}
         # warp the read sequence according to CIGAR
-        mod_id = 0
-        start = start_pos
-        for pair_info in parsed_cigar:
-            code, runs = pair_info
-            if code == 0: # M
-                mod_sequence += sequence[mod_id:mod_id + runs]
-                mod_id += runs
-            elif code == 1: # I
-                mod_id += runs
-            elif code == 2: # D
-                mod_sequence += '-'*runs
-            elif code == 4 or code == 5: # S or H, pysam already parsed
-                pass
-            else:
-                print ("ERROR: unexpected cigar code", segment.cigarstring)
+        mod_sequence = warp_by_cigar(cigar_tuples, sequence, seq_name, dict_chr_var_gap[ref_name], start_pos)
         try:
             assert len(mod_sequence) == (segment.reference_end - start_pos)
         except:
             print("WARNING! Warping fail at:", segment.query_name)
         
-        if ref_name not in chr_sam.keys():
-            chr_sam[ref_name] = [[], [], [], []]
         chr_sam[ref_name][0].append(start_pos) #position
         chr_sam[ref_name][1].append(mod_sequence) #sequence
         chr_sam[ref_name][2].append(mapq)
         chr_sam[ref_name][3].append(rg_tag)
     in_sam_file.close()
-    return chr_sam
+    return chr_sam, dict_chr_var_gap
 
 
 
 def compare_vcf_sam(fn_vcf, fn_sam, fn_output):
-    chr_vcf = parse_vcf_to_dict(fn_vcf)
-    chr_sam = parse_sam_to_dict(fn_sam)
+    chr_SNP_vcf, chr_gap_vcf  = parse_vcf_to_dict(fn_vcf)
+    chr_sam, dict_chr_var_gap = parse_sam_to_dict(fn_sam)
 
-    # COMPARE VCF AND SAM FILE
+    # COMPARE VCF AND SAM FILE FOR SNPs
     f = open(fn_output, 'w')
     f.write("CHR\tHET_SITE\tREFERENCE_BIAS\tREF_COUNT\tALT_COUNT\tGAP_COUNT\tOTHER_COUNT\tNUM_READS\tSUM_MAPQ\tREAD_DISTRIBUTION\n")
     
-    for ref_name, list_het_info in sorted(chr_vcf.items()):
+    for ref_name, list_het_info in sorted(chr_SNP_vcf.items()):
         list_sam_pos   = chr_sam[ref_name][0]
         list_sam_reads = chr_sam[ref_name][1]
         list_mapq      = chr_sam[ref_name][2]
@@ -179,6 +209,14 @@ def compare_vcf_sam(fn_vcf, fn_sam, fn_output):
         print("total other base:", total_other_count)
     f.close()
 
+    # Prepare for the gapped reference bias analysis
+    """ 
+    for ref_name, list_variant in sorted(chr_gap_vcf.items()):
+        for list_info_pairs in list_variant:
+            pos, ref, list_alt, hap_info = list_info_pairs
+            if dict_chr_var_gap[ref_name].get(pos):
+                print(ref, list_alt, hap_info, dict_chr_var_gap[ref_name][pos])
+    """
 
 
 if __name__ == "__main__":
