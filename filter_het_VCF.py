b95bd71eade4ad9e948aa2671425c101a32cfff0
maojanlin <mlin77@jhu.edu>
2022-01-02 21:35:15 -0500

    modify to consider all non '0|0' variants

--- filter_het_VCF.py
+++ filter_het_VCF.py
@@ -6,8 +6,10 @@ def parse_het_site(fn_vcf, fn_output):
     in_vcf_file  = pysam.VariantFile(fn_vcf, 'r')
     out_vcf_file = pysam.VariantFile(fn_output, 'w', header=in_vcf_file.header)
     for segment in in_vcf_file:
-        hap_info = str(segment).split()[9].split('|') # "0|0", "1|0", "0|1" tag
-        if hap_info[0] != hap_info[1]:
+        #hap_info = str(segment).split()[9].split('|') # "0|0", "1|0", "0|1" tag
+        #if hap_info[0] != hap_info[1]:
+        hap_0, hap_1 = segment.samples[0]['GT']
+        if hap_0 + hap_1 != 0:
             out_vcf_file.write(segment)
     in_vcf_file.close()
     out_vcf_file.close()
